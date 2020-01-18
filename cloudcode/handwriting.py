492283ac65873f62c26c14f09a57836014a8aff0
Martin Chak <Martin.Chak@mail.utoronto.ca>
2020-01-18 18:32:30 -0500

    Implemented handwriting api to google cloud

---
+++ cloudcode/handwriting.py
@@ -0,0 +1,39 @@
+import os, io
+from google.cloud import vision
+from google.cloud.vision import types
+import json
+
+#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"keys.json"
+client = vision.ImageAnnotatorClient()
+
+def handWriting_OCR(file_info):
+    """Detects document features in an image."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    image = vision.types.Image(content=file_info)
+
+    response = client.document_text_detection(image=file_info)
+
+    print(response)
+
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            print('\nBlock confidence: {}\n'.format(block.confidence))
+
+            for paragraph in block.paragraphs:
+                print('Paragraph confidence: {}'.format(
+                    paragraph.confidence))
+
+                for word in paragraph.words:
+                    word_text = ''.join([
+                        symbol.text for symbol in word.symbols
+                    ])
+                    print('Word text: {} (confidence: {})'.format(
+                        word_text, word.confidence))
+
+                    for symbol in word.symbols:
+                        print('\tSymbol: {} (confidence: {})'.format(
+                            symbol.text, symbol.confidence))
+    print (response.full_text_annotation)
--- cloudcode/main.py
+++ cloudcode/main.py
@@ -1,6 +1,7 @@
 import sys
 
 from MathPix import handToMath as get_math
+from handwriting import handWriting_OCR as get_writing
 from google.cloud import storage
 
 def hello_gcs(event, context):
@@ -9,6 +10,19 @@ def hello_gcs(event, context):
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
+    image_bytes = read_image(event, context)
+
+    math = math_pix(image_bytes)
+    print("Math")
+    print(math)
+
+    writing = hand_write(image_bytes)
+    print("Writing")
+    print(writing)
+
+    return writing
+
+def read_image(event, context):
     client = storage.Client()
 
     # Get the file that has been uploaded to GCS
@@ -16,8 +30,14 @@ def hello_gcs(event, context):
     blob = bucket.get_blob(event['name'])
     imagedata = blob.download_as_string()
 
+    return imagedata
+
+def math_pix(imagedata):
     image_info = get_math(imagedata)
     return image_info
 
+def hand_write(imagedata):
+    image_info = get_writing(imagedata)
+    return image_info
 
 sys.modules[__name__] = hello_gcs
