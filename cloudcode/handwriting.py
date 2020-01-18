ee365ea14c5b43db4dabfbc594b449d5807a3d66
Martin Chak <Martin.Chak@mail.utoronto.ca>
2020-01-18 18:50:37 -0500

    Ammends to env variables

--- cloudcode/handwriting.py
+++ cloudcode/handwriting.py
@@ -14,7 +14,7 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = {
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/ocr-uofthacks%40ml-webapp-uzairmahmed.iam.gserviceaccount.com"
-}
+  }
 
 client = vision.ImageAnnotatorClient()
 
