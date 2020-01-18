9146e8d620a81840d3eaf7e3f6b379707858775a
Martin Chak <Martin.Chak@mail.utoronto.ca>
2020-01-18 15:56:23 -0500

    Put back the storage read code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 from MathPix import handToMath
+import cloudstorage
 
 def hello_gcs(event, context):
     """Triggered by a change to a Cloud Storage bucket.
@@ -17,5 +18,16 @@ def hello_gcs(event, context):
     print('Updated: {}'.format(event['updated']))
 
     file_name = '/' + event['bucket'] + '/' + event['name']
+
+    gcs_file = cloudstorage.open(file_name)
+    contents = gcs_file.read()
+    gcs_file.close()
+
+    file_info = send_file(io.BytesIO(contents), mimetype='image/png')
+
+    print(file_info)
+
+    return file_info
+
 
 sys.modules[__name__] = hello_gcs
