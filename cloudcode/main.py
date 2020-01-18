30d1f74f8c5fdc147a94215765d4fec4b19eccaa
Martin Chak <Martin.Chak@mail.utoronto.ca>
2020-01-18 13:33:57 -0500

    Created cloud code

---
+++ cloudcode/main.py
@@ -0,0 +1,16 @@
+from MathPix import handToMath as get_math
+
+def hello_gcs(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    
+    print('Event ID: {}'.format(context.event_id))
+    print('Event type: {}'.format(context.event_type))
+    print('Bucket: {}'.format(event['bucket']))
+    print('File: {}'.format(event['name']))
+    print('Metageneration: {}'.format(event['metageneration']))
+    print('Created: {}'.format(event['timeCreated']))
+    print('Updated: {}'.format(event['updated']))
