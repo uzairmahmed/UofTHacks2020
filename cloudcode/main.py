4699db487f98e0157b723abc77190c728228ec0c
Martin Chak <Martin.Chak@mail.utoronto.ca>
2020-01-18 14:00:43 -0500

    revert to previous version

---
+++ cloudcode/main.py
@@ -0,0 +1,11 @@
+from MathPix import handToMath as get_math
+
+def hello_gcs(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    file = event
+    print(f"Processing file: {file['name']}.")
+    print(f"{file['msg']}")
