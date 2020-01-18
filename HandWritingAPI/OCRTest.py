611c6854921e8a8b266cfa80515927d85ffb4816
Uzair <uzairmahmed@gmail.com>
2020-01-18 17:53:30 -0500

    Handwriting demo done

---
+++ HandWritingAPI/OCRTest.py
@@ -0,0 +1,47 @@
+import os, io
+from google.cloud import vision
+from google.cloud.vision import types
+import json
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"keys.json"
+client = vision.ImageAnnotatorClient()
+
+file_name = 'testingImages/test9.png'
+
+def handWriting_OCR(path):
+    """Detects document features in an image."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+
+    print(response)
+
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            print('\nBlock confidence: {}\n'.format(block.confidence))
+
+            for paragraph in block.paragraphs:
+                print('Paragraph confidence: {}'.format(
+                    paragraph.confidence))
+
+                for word in paragraph.words:
+                    word_text = ''.join([
+                        symbol.text for symbol in word.symbols
+                    ])
+                    print('Word text: {} (confidence: {})'.format(
+                        word_text, word.confidence))
+
+                    for symbol in word.symbols:
+                        print('\tSymbol: {} (confidence: {})'.format(
+                            symbol.text, symbol.confidence))
+    print (response.full_text_annotation)
+
+
+handWriting_OCR(file_name)
