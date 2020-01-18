70401bc996328666feddf7389c5baab23f0c158e
Martin Chak <Martin.Chak@mail.utoronto.ca>
2020-01-18 14:29:37 -0500

    probing in main

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ def hello_gcs(event, context):
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
-    file = event
-    print(f"Processing file: {file['name']}.")
+    print('Event ID: {}'.format(context.event_id))
+    print('Event type: {}'.format(context.event_type))
+    print('Bucket: {}'.format(event['bucket']))
+    print('File: {}'.format(event['name']))
+    print('Metageneration: {}'.format(event['metageneration']))
+    print('Created: {}'.format(event['timeCreated']))
+    print('Updated: {}'.format(event['updated']))
 
 sys.modules[__name__] = hello_gcs
