44055219502cb233fb9f9aca5a20a6005d17cf69
Martin Chak <Martin.Chak@mail.utoronto.ca>
2020-01-18 21:22:24 -0500

    removed print statements in handwriting.py

--- cloudcode/handwriting.py
+++ cloudcode/handwriting.py
@@ -13,28 +13,4 @@ def handWriting_OCR(file_info):
 
     response = client.document_text_detection(image=image)
 
-    #print(response)
-    """
-    for page in response.full_text_annotation.pages:
-        for block in page.blocks:
-            print('\nBlock confidence: {}\n'.format(block.confidence))
-
-            for paragraph in block.paragraphs:
-                print('Paragraph confidence: {}'.format(
-                    paragraph.confidence))
-
-                for word in paragraph.words:
-                    word_text = ''.join([
-                        symbol.text for symbol in word.symbols
-                    ])
-                    print('Word text: {} (confidence: {})'.format(
-                        word_text, word.confidence))
-
-                    for symbol in word.symbols:
-                        print('\tSymbol: {} (confidence: {})'.format(
-                            symbol.text, symbol.confidence))
-    """
-    #print (response.full_text_annotation)
-    print("finished handwriting")
-
     return response
