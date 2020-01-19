8ad1fe6f2b56a900b641170e07e5aede83a278ec
Martin Chak <Martin.Chak@mail.utoronto.ca>
2020-01-19 02:14:16 -0500

    Allowed firebase initialization

--- cloudcode/main.py
+++ cloudcode/main.py
@@ -12,10 +12,10 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # FIREBASE
-#MAIN_COLLECTION=u'Documents'
-#cred = credentials.Certificate("serviceAccountKey.json")
-#firebase_admin.initialize_app(cred)
-#db = firestore.client()
+MAIN_COLLECTION=u'Documents'
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 # GCS
 client = storage.Client()
