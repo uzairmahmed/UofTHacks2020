3818d0f28fa057e241ce5cd1f3f7966d209ec320
Martin Chak <Martin.Chak@mail.utoronto.ca>
2020-01-18 21:29:12 -0500

    Added time probes

--- cloudcode/handwriting.py
+++ cloudcode/handwriting.py
@@ -7,10 +7,14 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"keys.json"
 
 def handWriting_OCR(file_info):
     """Detects document features in an image."""
+    print("Start Writing")
+
     client = vision.ImageAnnotatorClient()
 
     image = vision.types.Image(content=file_info)
 
     response = client.document_text_detection(image=image)
+
+    print("End Writing")
 
     return response
